a23f2dc24233ce23775ad52c84bc740b99a65695
move style to head <timmortal44@yahoo.com>
2022-12-29 22:13:05 +0100

    import featured productsss

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,6 +2,6 @@ import React from 'react';
 import Slider from "../../components/Slider/Slider";
 import "../../components/FeaturedProducts/FeaturedProducts"
 import "./Home.scss";
-
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts';
 
 
