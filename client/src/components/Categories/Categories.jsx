5438f602ea2ca0afbd950849f4bcbee35def9337
move style to head <timmortal44@yahoo.com>
2022-12-30 22:42:32 +0100

    add link ro button

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -9,7 +9,7 @@ const Categories = () => {
             <div className="row">
                 <img src="https://images.pexels.com/photos/2887766/pexels-photo-2887766.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
                 <button>
-                 
+                    <Link className='link' to="/products/1">Sale</Link>
                 </button>
             </div>
 
