b472658cef54bbd8a48f9fbaedbaad6a8f23fd8f
move style to head <timmortal44@yahoo.com>
2022-12-30 22:42:20 +0100

    add button

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -8,7 +8,9 @@ const Categories = () => {
         <div className="col">
             <div className="row">
                 <img src="https://images.pexels.com/photos/2887766/pexels-photo-2887766.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-              
+                <button>
+                 
+                </button>
             </div>
 
         </div>
