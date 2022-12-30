b1f31c9cf9a6f9383c77eefde91f331e41e8566d
move style to head <timmortal44@yahoo.com>
2022-12-31 00:09:39 +0100

    include btn

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -51,7 +51,9 @@ const Categories = () => {
 
             <div className="row">
             <img src="https://images.pexels.com/photos/5242790/pexels-photo-5242790.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                
+                <button>
+                    
+                </button>
             </div>
         </div>
     </div>
