7f7e4c759eee2e1ba013a1745abd1713d994d29c
move style to head <timmortal44@yahoo.com>
2022-12-31 23:48:00 +0100

    imprt ggl

--- client/src/components/Contacts/Contacts.jsx
+++ client/src/components/Contacts/Contacts.jsx
@@ -3,6 +3,7 @@ import './Contacts.scss';
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
+import GoogleIcon from "@mui/icons-material/Google";
 
 
 
