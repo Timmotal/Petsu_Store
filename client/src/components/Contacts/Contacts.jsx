05f020d516cd44444687442fcba12c30d31ef0d0
move style to head <timmortal44@yahoo.com>
2022-12-31 23:47:50 +0100

    imprt twt

--- client/src/components/Contacts/Contacts.jsx
+++ client/src/components/Contacts/Contacts.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './Contacts.scss';
 import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
+import TwitterIcon from "@mui/icons-material/Twitter";
 
 
 
