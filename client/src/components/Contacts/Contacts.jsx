acfb30d9ebc6e56c13e1464f14dd817d7fa43a98
move style to head <timmortal44@yahoo.com>
2022-12-31 23:48:11 +0100

    imprt ptrst

--- client/src/components/Contacts/Contacts.jsx
+++ client/src/components/Contacts/Contacts.jsx
@@ -4,6 +4,6 @@ import FacebookIcon from "@mui/icons-material/Facebook";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import GoogleIcon from "@mui/icons-material/Google";
-
+import PinterestIcon from "@mui/icons-material/Pinterest";
 
 
