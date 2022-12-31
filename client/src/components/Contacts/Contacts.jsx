17cb3dec64a9f8dae4af1d1830940f752bc50de1
move style to head <timmortal44@yahoo.com>
2022-12-31 23:48:59 +0100

    add input type

--- client/src/components/Contacts/Contacts.jsx
+++ client/src/components/Contacts/Contacts.jsx
@@ -12,7 +12,7 @@ const Contacts = () => {
         <div className="wrapper">
             <span>be in touch with us:</span>
             <div className="mail">
-            
+                <input type="text" placeholder='Enter Your Email...' />    
             </div>
           
         </div>
