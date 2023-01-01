5fe8f80f4a5bf338bbd5373ed5fd513a60369323
move style to head <timmortal44@yahoo.com>
2023-01-01 23:03:50 +0100

    add first ibj to array

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -5,7 +5,15 @@ import './FeaturedProducts.scss'
 const FeaturedProducts = ({ type }) => {
 
     const data = [
-       
+        {
+            id: 1,
+            img: "https://images.pexels.com/photos/1936854/pexels-photo-1936854.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            img2: "https://images.pexels.com/photos/859195/pexels-photo-859195.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Long Sleeve",
+            isNew: true,
+            oldPrice: 110,
+            price: 100,
+        },
       
        
     ]
