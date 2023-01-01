00448d05b6f96046b402f3d5a62047fa96f12ac8
move style to head <timmortal44@yahoo.com>
2023-01-01 23:06:22 +0100

    include card comp

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -61,7 +61,7 @@ const FeaturedProducts = ({ type }) => {
             </div>
             <div className="bottom">
                 {data.map( item => (
-                
+                    <Card item={item} key={item.id} />
                 ))}
             </div>
         </div>
