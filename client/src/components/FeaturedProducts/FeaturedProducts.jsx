a0949c42a8e7d4083daffd21697deca264e77124
move style to head <timmortal44@yahoo.com>
2023-01-01 23:05:50 +0100

    add text to p tags

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -53,7 +53,10 @@ const FeaturedProducts = ({ type }) => {
             <div className="top">
                 <h1>{type} products</h1>
                 <p>
-                 
+                    Even the all-powerful Pointing
+                    has no control about the blind texts it is an almost unorthographic
+                    life One day however a small line of blind text by the name of Lorem
+                    Ipsum decided to leave for the far World of Grammar.
                 </p>
             </div>
            
