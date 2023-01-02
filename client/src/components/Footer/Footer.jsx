e556681f2e7f2b6e662862115b231cfd1ae12780
move style to head <timmortal44@yahoo.com>
2023-01-02 23:56:15 +0100

    add more span

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -46,6 +46,9 @@ const Footer = () => {
             <div className="item">
                 <h1>Contact</h1>
                 <span>
+                      Far far away, behind the word mountains, far from the countries Vokalia
+                      and Consonantia, there live the blind texts. Separated they live in
+                      Bookmarksgrove right at the coast of the Semantics, a large language ocean.
                   </span>
             </div>
         </div>
