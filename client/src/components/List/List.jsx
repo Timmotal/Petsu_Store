1f21b07e0e970499375ed92b0c73fbacf8bcd727
move style to head <timmortal44@yahoo.com>
2023-01-03 23:10:30 +0100

    add first obj top array

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -5,6 +5,15 @@ import "./List.scss";
 const List = () => {
 
     const data = [
+        {
+            id: 1,
+            img: "https://images.pexels.com/photos/1936854/pexels-photo-1936854.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            img2: "https://images.pexels.com/photos/859195/pexels-photo-859195.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Long Sleeve",
+            isNew: true,
+            oldPrice: 110,
+            price: 100,
+        },
         
         
     ]
