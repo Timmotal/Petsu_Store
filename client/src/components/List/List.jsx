bc37e03f472b46d1bd484cc81658d8f3528d06a8
move style to head <timmortal44@yahoo.com>
2023-01-03 23:11:26 +0100

    add th obj to array

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -45,7 +45,14 @@ const List = () => {
             oldPrice: 510,
             price: 500,
         },
-        
+        {
+            id: 6,
+            img: "https://images.pexels.com/photos/6976660/pexels-photo-6976660.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            title: "Skirt",
+            isNew: true,
+            oldPrice: 510,
+            price: 500,
+        },
     ]
 
   
