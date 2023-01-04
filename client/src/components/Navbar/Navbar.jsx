13081bd17406b71a9ed89e55e1e8dc3ee5944a4f
move style to head <timmortal44@yahoo.com>
2023-01-04 23:03:21 +0100

    import cart comp

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 import { Link } from 'react-router-dom';
 import "./Navbar.scss"
-
+import Cart from '../Cart/Cart';
 
 
 
