8ac028c54c098648f4a83cf3201a676b96644a49
move style to head <timmortal44@yahoo.com>
2023-01-04 23:02:07 +0100

    import person outline icon

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 
 
 
