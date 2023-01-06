67c7418c5afb04b10716d0748a6e6fcabdf4f5c0
move style to head <timmortal44@yahoo.com>
2023-01-06 23:27:04 +0100

    init curretSlide

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -4,7 +4,9 @@ import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
 
 import "./Slider.scss";
 
- 
+const Slider = () => {
+
+    const [currentSlide, setCurrentSlide] = useState(0);
 
   
   
