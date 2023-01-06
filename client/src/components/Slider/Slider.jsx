5a7f3b99596fb3d63fabb3a277305d41512f6bfd
move style to head <timmortal44@yahoo.com>
2023-01-06 23:27:16 +0100

    init data

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -8,7 +8,14 @@ const Slider = () => {
 
     const [currentSlide, setCurrentSlide] = useState(0);
 
-  
+    const data = [
+        "https://images.unsplash.com/photo-1663079974166-b154aac575d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80",
+        "https://images.unsplash.com/photo-1661768662059-4c4edfd09346?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80",
+        "https://images.unsplash.com/photo-1669072257062-fc92a408ff8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80",
+        "https://images.unsplash.com/photo-1670116425207-7ff3a28c6a08?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80",
+        "https://images.unsplash.com/photo-1670115982436-b77e392a39f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1600&q=80"
+    ];
+
   
   
 
