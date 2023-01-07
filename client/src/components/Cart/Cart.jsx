13ebd0db84058de6760b772efa2c71bdeec1a415
move style to head <timmortal44@yahoo.com>
2023-01-07 23:33:32 +0100

    include first obj

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -5,7 +5,15 @@ import DeleteOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 const Cart = () => {
 
     const data = [
-       ,
+        {
+            id: 1,
+            img: "https://images.pexels.com/photos/1390600/pexels-photo-1390600.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Pyjamas",
+            desc: "Pyjamas",
+            isNew: true,
+            oldPrice: 410,
+            price: 400,
+        },
        
     ]
 
