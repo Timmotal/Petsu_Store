bf91f557dbc10f31da823afa1ead562d18143845
move style to head <timmortal44@yahoo.com>
2023-01-07 23:33:40 +0100

    add second obj

--- client/src/components/Cart/Cart.jsx
+++ client/src/components/Cart/Cart.jsx
@@ -14,7 +14,15 @@ const Cart = () => {
             oldPrice: 410,
             price: 400,
         },
-       
+        {
+            id: 2,
+            img: "https://images.pexels.com/photos/2468231/pexels-photo-2468231.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+            title: "Palazo",
+            isNew: true,
+            desc: "Pyjamas",
+            oldPrice: 510,
+            price: 500,
+        },
     ]
 
 
