eab786b6fd2be0130b4985332f2b78f80397c811
move style to head <timmortal44@yahoo.com>
2023-01-09 23:26:03 +0100

    import BalanceIcon

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import "./Product.scss";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import BalanceIcon from "@mui/icons-material/Balance";
+
 
 
 
