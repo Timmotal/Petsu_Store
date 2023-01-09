d0ccf445fc83d96e74c8f19f1d9cae734b198460
move style to head <timmortal44@yahoo.com>
2023-01-09 23:25:48 +0100

    import FavoriteBorderIcons

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import "./Product.scss";
 import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
 
 
 
