f919391772d1e75acd416ab4315a30416f04815b
move style to head <timmortal44@yahoo.com>
2023-01-10 23:42:55 +0100

    include first img

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -11,6 +11,7 @@ const Product = () => {
   const [quantity, setQuantity] = useState(1);
 
   const images = [
+    "https://images.pexels.com/photos/4685042/pexels-photo-4685042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   ]
 
  
