2d209c0b86db641dd8961752df49d1201f575dbd
move style to head <timmortal44@yahoo.com>
2023-01-10 23:43:09 +0100

    add 3rd img

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -13,6 +13,7 @@ const Product = () => {
   const images = [
     "https://images.pexels.com/photos/4685042/pexels-photo-4685042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     "https://images.pexels.com/photos/4685031/pexels-photo-4685031.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/4685034/pexels-photo-4685034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
   ]
 
  
