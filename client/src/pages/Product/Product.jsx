d98a3979b4e7e9d47e849a1160cdd5078377c750
move style to head <timmortal44@yahoo.com>
2023-01-10 23:43:15 +0100

    add 4th img

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -14,6 +14,7 @@ const Product = () => {
     "https://images.pexels.com/photos/4685042/pexels-photo-4685042.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     "https://images.pexels.com/photos/4685031/pexels-photo-4685031.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     "https://images.pexels.com/photos/4685034/pexels-photo-4685034.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/4685033/pexels-photo-4685033.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   ]
 
  
