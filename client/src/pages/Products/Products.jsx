47dab56beb6b2e1a8de4a7298d0494803c343167
move style to head <timmortal44@yahoo.com>
2023-02-05 22:51:56 +0100

    import product sass file

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import List from '../../components/List/List';
 import useFetch from '../../hooks/useFetch';
+import "./Products.scss";
 
 
 
