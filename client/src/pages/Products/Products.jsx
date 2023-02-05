37ca1a4320af0180a1ae34faf05ee4698b8e6e3d
move style to head <timmortal44@yahoo.com>
2023-02-05 22:51:04 +0100

    import useFetch

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import List from '../../components/List/List';
+import useFetch from '../../hooks/useFetch';
 
 
 
