119bf72d19d2290c88fbf4f079a0ccf299a4c2a5
move style to head <timmortal44@yahoo.com>
2023-02-19 23:46:42 +0100

    init loading varariable

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -4,7 +4,7 @@ import { makeRequest } from '../makeRequest/makeRequest';
 const useFetch = (url) => { // here we used "ASYNC" twice, it gave us errors
 
     const [data, setData] = useState(null);
-   
+    const [loading, setLoading] = useState(false);
 
 
     //refactoring our code base because we use useEffect a lot of times .....
