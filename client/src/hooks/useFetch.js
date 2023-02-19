0d407e3186f37d96ce7e946640f8d8abf39522a3
move style to head <timmortal44@yahoo.com>
2023-02-19 23:46:59 +0100

    init error variable

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -5,7 +5,7 @@ const useFetch = (url) => { // here we used "ASYNC" twice, it gave us errors
 
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(false);
-
+    const [error, setError] = useState(false);
 
     //refactoring our code base because we use useEffect a lot of times .....
 
