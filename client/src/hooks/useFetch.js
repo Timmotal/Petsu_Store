c1897ee8ad1521e8443b82115e3c5a05365caf34
move style to head <timmortal44@yahoo.com>
2023-02-19 23:46:11 +0100

    make func exp for use fetch

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,3 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { makeRequest } from '../makeRequest/makeRequest';
 
+const useFetch = (url) => { // here we used "ASYNC" twice, it gave us errors
+
+
+   
+
+
+    //refactoring our code base because we use useEffect a lot of times .....
+
+    ;
+
+}
+
